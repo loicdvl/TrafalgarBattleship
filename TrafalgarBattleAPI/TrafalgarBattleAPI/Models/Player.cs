ee5faaf2ae8f3a356d7cf91cedd89423da1e6af9
loicdvl <loic.devoille@hotmail.fr>
2017-03-02 10:08:24 +0100

    WebAPI - Ajout erreur de commit

--- TrafalgarBattleAPI/TrafalgarBattleAPI/Models/Player.cs
+++ TrafalgarBattleAPI/TrafalgarBattleAPI/Models/Player.cs
@@ -7,21 +7,34 @@ namespace TrafalgarBattleAPI.Models
 {
     public class Player
     {
+        private static int idPlayer { get; set; }
         private string pseudo { get; set; }
         private int shipNb { get; set; }
         private int score { get; set; }
         private bool isDisponible { get; set; }
+
+        public Player()
+        {
+            pseudo = "Anonyme";
+        }
+        
+        public bool createAccount(string pseudo, string mdp)
+        {
+
+            return true;
+        }
+
+        public bool login(string pseudo, string mdp)
         {
             return true;
         }
 
-        {
-        }
-
+        public void challengePlayer(Player p)
         {
 
         }
 
+        public void acceptChallenge(Player j)
         {
 
         }
