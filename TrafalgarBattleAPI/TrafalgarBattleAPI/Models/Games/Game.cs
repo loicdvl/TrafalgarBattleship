245c7cb0b9cbd3c69eebf4007f73129690f8bf07
loicdvl <loic.devoille@hotmail.fr>
2017-04-09 14:50:36 +0200

    resolve #5

---
+++ TrafalgarBattleAPI/TrafalgarBattleAPI/Models/Games/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrafalgarBattleAPI.Models.Games
+{
+    public class Game
+    {
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+
+        public Game()
+        {
+
+        }
+    }
+}
