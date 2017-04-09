4c49dbe151c0fdf960d07ee3c3d1689e8ac55b54
loicdvl <loic.devoille@hotmail.fr>
2017-04-09 14:48:21 +0200

    resolve #4

---
+++ TrafalgarBattleAPI/TrafalgarBattleAPI/Models/Boards/Case.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrafalgarBattleAPI.Models
+{
+    public class Case
+    {
+        public Coordinates Coordinates { get; set; }
+
+        public Case(int row, int column)
+        {
+            Coordinates = new Coordinates(row, column);
+        }
+
+        public bool IsOccupied
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ TrafalgarBattleAPI/TrafalgarBattleAPI/Models/Boards/PlayerGrid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrafalgarBattleAPI.Models
+{
+    public class PlayerGrid
+    {
+        public List<Case> Cases { get; set; }
+
+        public PlayerGrid()
+        {
+            Cases = new List<Case>();
+            for(int i=0;i<10;i++)
+            {
+                for(int j=0;j<10;j++)
+                {
+                    Cases.Add(new Case(i,j));
+                }
+            }
+        }
+    }
+}
