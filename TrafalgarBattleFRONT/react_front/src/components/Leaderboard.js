956dfd4eb62e0a1015edcefd8307aad11563a208
lucieseghini <lucieseurin.pro@gmail.com>
2017-05-11 11:41:06 +0200

    FRONT - leaderboard react component

--- TrafalgarBattleFRONT/react_front/src/components/Leaderboard.js
+++ TrafalgarBattleFRONT/react_front/src/components/Leaderboard.js
@@ -15,31 +15,27 @@ class Leaderboard extends React.Component {
             <div>
                 <Header />
 
-			    <!-- Défier un joueur -->
                 <Jumbotron className="jumbotron text-center">
 					<div id="header">
 						<h1>Trafalgar Battleship</h1>
 					</div>
 				</Jumbotron>
 				
-				<div class="test">
-					<!-- Bouton back to top -->
+				<div className="test">
 					<div id="elevator_item"><a id="elevator" onclick="return false;" title="Back To Top"></a></div>
 					
 					<div id="DefierJoueur">
 						<h1>Leaderboard : Classement des meilleurs joueurs !</h1>
-						<!-- Formulaire de recherche de joueur -->
-						<<FormControl className="form-inline">
-							<div class="form-group">
-								<label class="sr-only" for="PseudoJoueur">PseudoJoueur</label>
-								<input type="text" class="form-control" id="PseudoJoueur" placeholder="PseudoJoueur">
+						<FormControl className="form-inline">
+							<div className="form-group">
+								<label className="sr-only" for="PseudoJoueur">PseudoJoueur</label>
+								<input type="text" className="form-control" id="PseudoJoueur" placeholder="PseudoJoueur"/>
 							</div>
-							<button type="submit" class="btn btn-primary">Rechercher</button>
-							<button type="submit" class="btn btn-primary">Mon classement</button>
-						</<FormControl>
-						<!-- Tableau des joueurs en ligne -->
-						<div class="bs-example">
-							<table class="table table-striped">
+							<button type="submit" className="btn btn-primary">Rechercher</button>
+							<button type="submit" className="btn btn-primary">Mon classement</button>
+						</FormControl>
+						<div className="bs-example">
+							<table className="table table-striped">
 								<thead>
 									<tr>
 										<th></th>
@@ -52,7 +48,7 @@ class Leaderboard extends React.Component {
 								</thead>
 								<tbody>
 									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
+										<td><img alt="Brand" src="img/oldship.png" id="brand"/></td>
 										<td>1</td>
 										<td>John</td>
 										<td>10</td>
@@ -60,7 +56,7 @@ class Leaderboard extends React.Component {
 										<td>11</td>
 									</tr>
 									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
+										<td><img alt="Brand" src="img/oldship.png" id="brand"/></td>
 										<td>1</td>
 										<td>John</td>
 										<td>10</td>
@@ -68,7 +64,7 @@ class Leaderboard extends React.Component {
 										<td>11</td>
 									</tr>
 									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
+										<td><img alt="Brand" src="img/oldship.png" id="brand"/></td>
 										<td>1</td>
 										<td>John</td>
 										<td>10</td>
@@ -76,95 +72,7 @@ class Leaderboard extends React.Component {
 										<td>11</td>
 									</tr>
 									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
-										<td>1</td>
-										<td>John</td>
-										<td>10</td>
-										<td>1</td>
-										<td>11</td>
-									</tr>
-									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
-										<td>1</td>
-										<td>John</td>
-										<td>10</td>
-										<td>1</td>
-										<td>11</td>
-									</tr>
-									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
-										<td>1</td>
-										<td>John</td>
-										<td>10</td>
-										<td>1</td>
-										<td>11</td>
-									</tr>
-									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
-										<td>1</td>
-										<td>John</td>
-										<td>10</td>
-										<td>1</td>
-										<td>11</td>
-									</tr>
-									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
-										<td>1</td>
-										<td>John</td>
-										<td>10</td>
-										<td>1</td>
-										<td>11</td>
-									</tr>
-									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
-										<td>1</td>
-										<td>John</td>
-										<td>10</td>
-										<td>1</td>
-										<td>11</td>
-									</tr>
-									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
-										<td>1</td>
-										<td>John</td>
-										<td>10</td>
-										<td>1</td>
-										<td>11</td>
-									</tr>
-									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
-										<td>1</td>
-										<td>John</td>
-										<td>10</td>
-										<td>1</td>
-										<td>11</td>
-									</tr>
-									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
-										<td>1</td>
-										<td>John</td>
-										<td>10</td>
-										<td>1</td>
-										<td>11</td>
-									</tr>
-									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
-										<td>1</td>
-										<td>John</td>
-										<td>10</td>
-										<td>1</td>
-										<td>11</td>
-									</tr>
-									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
-										<td>1</td>
-										<td>John</td>
-										<td>10</td>
-										<td>1</td>
-										<td>11</td>
-									</tr>
-									<tr>
-										<td><img alt="Brand" src="img/oldship.png" id="brand"></td>
+										<td><img alt="Brand" src="img/oldship.png" id="brand"/></td>
 										<td>1</td>
 										<td>John</td>
 										<td>10</td>
@@ -177,15 +85,6 @@ class Leaderboard extends React.Component {
 					</div>
 				</div>
 				
-			    <!-- pied de page -->
-			    <footer>
-			        <div class="footer-info">
-			            <div class="footer-brand">
-			                <p> Loïc DEVOILLE & Lucie SEURIN</p>
-							<p>2016-2017 M1 Miage Apprentissage</p>
-			            </div>
-			        </div>
-			    </footer>
             </div>
         )
     }
