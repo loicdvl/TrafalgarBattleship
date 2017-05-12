74da523552b615c45fb012b6fab240ecdb6f82e6
loicdvl <loic.devoille@hotmail.fr>
2017-05-12 14:18:43 +0200

    Front - add Link to placing ships

--- TrafalgarBattleFRONT/react_front/src/components/views/OnlinePlayer.js
+++ TrafalgarBattleFRONT/react_front/src/components/views/OnlinePlayer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Image, Button } from 'react-bootstrap';
+import { Link } from 'react-router'
 
 class OnlinePlayer extends React.Component {
     render () {
@@ -9,7 +10,7 @@ class OnlinePlayer extends React.Component {
                 <td><Image src="../img/oldship.png" id="brand" /></td>
                 <td>{this.props.player.pseudo}</td>
                 <td>{this.props.player.score}</td>
-                <td><Button bsStyle="primary">Défier</Button></td>
+                <td><Link to="/game/placing-ships"><Button bsStyle="primary">Défier</Button></Link></td>
             </tr>
         )
     }
