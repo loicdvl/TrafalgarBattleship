5d721b8ffcea23b14678069ea7b7ae15e37b4a65
lucieseghini <lucieseurin.pro@gmail.com>
2017-05-18 09:59:21 +0200

    FRONT - Placing update + Create GridContainer

---
+++ TrafalgarBattleFRONT/react_front/src/components/containers/GridContainer.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+class GridContainer extends React.Component {
+    render() {
+
+        return (
+            <Grid bsClass="placingGrid">
+                <Row >
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                </Row>
+                <Row bsClass="row">
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                </Row>
+                <Row bsClass="row">
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                </Row>
+                <Row bsClass="row">
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                </Row>
+                <Row bsClass="row">
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                </Row>
+                <Row bsClass="row">
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                </Row>
+                <Row bsClass="row">
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                    <Col bsClass="col" sm={1} md={1} lg={1}/>
+                </Row>
+            </Grid>
+        )
+    }
+}
+
+export default GridContainer;
