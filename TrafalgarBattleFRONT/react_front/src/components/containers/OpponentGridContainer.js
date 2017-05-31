757a407619946509e982641e80cec05149bf67fc
loicdvl <loic.devoille@hotmail.fr>
2017-05-31 12:23:23 +0200

    Front - opponentGrid is cliquable

--- TrafalgarBattleFRONT/react_front/src/components/containers/OpponentGridContainer.js
+++ TrafalgarBattleFRONT/react_front/src/components/containers/OpponentGridContainer.js
@@ -10,10 +10,15 @@ class OpponentGridContainer extends React.Component {
         gridApi.getEmptyGrid();
     };
 
+    fire  = () => {
+        console.log("fire");
+    };
+
+
     renderCols = (cols, row) => {
         let line = [];
         for(let j = row; j < row+10; j++) {
-            line[j] = <Col bsClass="col" sm={1} md={1} lg={1} x={j-row} y={row} className={cols[j].IsOccupied ? "occupied" : "water"} key={row+j} />;
+            line[j] = <Col bsClass="col" sm={1} md={1} lg={1} x={j-row} y={row} className={cols[j].IsOccupied ? "occupied" : "water"} key={row+j} onClick={this.fire} />;
         }
         return line;
     };
