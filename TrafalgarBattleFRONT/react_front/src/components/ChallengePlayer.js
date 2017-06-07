b2edcebf5f0cca57c0f6c05dcfe09dbbf6ce174c
loicdvl <loic.devoille@hotmail.fr>
2017-06-07 21:37:16 +0200

    Front - OnlineUser for Challenge page

--- TrafalgarBattleFRONT/react_front/src/components/ChallengePlayer.js
+++ TrafalgarBattleFRONT/react_front/src/components/ChallengePlayer.js
@@ -1,12 +1,45 @@
 import React from 'react';
 
+import { hubConnection } from 'signalr-no-jquery';
+
 import SearchFormContainer from './containers/SearchFormContainer';
 import OnlinePlayerListContainer from './containers/OnlinePlayersListContainer';
 
 import '../css/bootstrap/css/bootstrap.min.css';
 import '../css/style.css';
+import {updateOnlineUserList} from "../api/online-users-api";
 
 class ChallengePlayer extends React.Component {
+    componentDidMount() {
+        const connection = hubConnection('http://localhost:54409');
+        const OnlineUserStoreProxy = connection.createHubProxy('OnlineUserStore');
+
+        let user = {
+            ConnectionId: 1,
+            Rank: 1,
+            Avatar: '/img/oldship.png',
+            Name: 'Loic',
+            Victory: 100,
+            Defeat: 0
+        };
+
+        connection.start()
+            .done(function () { console.log(`Now connected, connection ID=${connection.id}`) })
+            .done( () => {
+                OnlineUserStoreProxy.invoke('Connect',user);
+            });
+
+        OnlineUserStoreProxy.on('updateOnlineUserList', (_connections) => {
+            console.log('updateOnlineUserList() called with param',_connections);
+            updateOnlineUserList(_connections);
+        });
+    }
+
+    componentWillUnmount() {
+        // disconnect user
+        // OnlineUserStoreProxy.invoke('Disconnect',user);
+    }
+
     render() {
         return (
             <div className="test">
