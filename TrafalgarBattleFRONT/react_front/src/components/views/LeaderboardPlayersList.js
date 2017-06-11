20a00f0072e7b27654c9f5cc64c5317821d9da04
loicdvl <loic.devoille@hotmail.fr>
2017-06-11 16:50:05 +0200

    Front - fix table display bis

--- TrafalgarBattleFRONT/react_front/src/components/views/LeaderboardPlayersList.js
+++ TrafalgarBattleFRONT/react_front/src/components/views/LeaderboardPlayersList.js
@@ -5,36 +5,35 @@ import { Image } from 'react-bootstrap';
 class LeaderboardPlayersList extends React.Component {
 
     renderLeaderboardList () {
-        if (this.props.leaderboard.length > 1)
-        {
-            return this.props.leaderboard.map(player => {
+        if(this.props.leaderboard !== null) {
+            if (this.props.leaderboard.length > 1) {
+                return this.props.leaderboard.map(player => {
+                    return (
+                        <tr key={player.ConnectionId}>
+                            <td><Image src="../../img/oldship.png"/></td>
+                            <td>{player.Rank}</td>
+                            <td>{player.Name}</td>
+                            <td>{player.Victory}</td>
+                            <td>{player.Defeat}</td>
+                            <td>{player.Victory - player.Defeat}</td>
+                        </tr>
+                    );
+                })
+            }
+            else {
                 return (
-                    <tr key={player.Rank}>
+                    <tr key={this.props.leaderboard.ConnectionId}>
                         <td><Image src="../../img/oldship.png"/></td>
-                        <td>{player.Rank}</td>
-                        <td>{player.Name}</td>
-                        <td>{player.Victory}</td>
-                        <td>{player.Defeat}</td>
-                        <td>{player.Victory - player.Defeat}</td>
+                        <td>{this.props.leaderboard.Rank}</td>
+                        <td>{this.props.leaderboard.Name}</td>
+                        <td>{this.props.leaderboard.Victory}</td>
+                        <td>{this.props.leaderboard.Defeat}</td>
+                        <td>{this.props.leaderboard.Victory - this.props.leaderboard.Defeat}</td>
                     </tr>
-                );
-            })
+                )
+            }
         }
-        else if(this.props.leaderboard.length === 1)
-        {
-            return (
-                <tr key={this.props.leaderboard.Rank}>
-                    <td><Image src="../../img/oldship.png"/></td>
-                    <td>{this.props.leaderboard.Rank}</td>
-                    <td>{this.props.leaderboard.Name}</td>
-                    <td>{this.props.leaderboard.Victory}</td>
-                    <td>{this.props.leaderboard.Defeat}</td>
-                    <td>{this.props.leaderboard.Victory - this.props.leaderboard.Defeat}</td>
-                </tr>
-            )
-        }
-        else
-        {
+        else {
             return null;
         }
     }
--- TrafalgarBattleFRONT/react_front/src/components/views/OnlinePlayerList.js
+++ TrafalgarBattleFRONT/react_front/src/components/views/OnlinePlayerList.js
@@ -6,31 +6,31 @@ import { Link } from 'react-router'
 class OnlinePlayerList extends React.Component {
 
     renderOnlinePlayersList() {
-        if (this.props.players.length > 1 ){
-            return this.props.players.map((player) => {
+        if (this.props.players !== null){
+            if (this.props.players.length > 1 ) {
+                return this.props.players.map((player) => {
+                    return (
+                        <tr key={player.Name}>
+                            <td><Image src={player.Avatar} id="brand" /></td>
+                            <td>{player.Name}</td>
+                            <td>{player.Victory}</td>
+                            <td><Link to="/game/placing-ships"><Button bsStyle="primary">Défier</Button></Link></td>
+                        </tr>
+                    );
+                });
+            }
+            else {
                 return (
-                    <tr key={player.Name}>
-                        <td><Image src={player.Avatar} id="brand" /></td>
-                        <td>{player.Name}</td>
-                        <td>{player.Victory}</td>
+                    <tr key={this.props.players.Name}>
+                        <td><Image src={this.props.players.Avatar} id="brand" /></td>
+                        <td>{this.props.players.Name}</td>
+                        <td>{this.props.players.Victory}</td>
                         <td><Link to="/game/placing-ships"><Button bsStyle="primary">Défier</Button></Link></td>
                     </tr>
-                );
-            });
+                )
+            }
         }
-        else if (this.props.players.length === 1)
-        {
-            return (
-                <tr key={this.props.players.Name}>
-                    <td><Image src={this.props.players.Avatar} id="brand" /></td>
-                    <td>{this.props.players.Name}</td>
-                    <td>{this.props.players.Victory}</td>
-                    <td><Link to="/game/placing-ships"><Button bsStyle="primary">Défier</Button></Link></td>
-                </tr>
-            )
-        }
-        else
-        {
+        else {
             return null;
         }
     }
