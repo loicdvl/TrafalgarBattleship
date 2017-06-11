29a43a800d1060bce43d47b65809eea5f83bb27a
loicdvl <loic.devoille@hotmail.fr>
2017-06-11 14:55:09 +0200

    Front - creation user-reducer

---
+++ TrafalgarBattleFRONT/react_front/src/reducers/user-reducer.js
@@ -0,0 +1,21 @@
+import * as types from '../actions/action-types';
+
+const initialState = {
+    user: {
+        Avatar: '/img/oldship.png',
+        Name: '#Anon'
+    }
+};
+
+const userReducer = function(state = initialState, action) {
+
+    switch(action.type) {
+
+        case types.GET_USER_SUCCESS:
+            return Object.assign({}, state, { user: action.user });
+        default:
+            return state;
+    }
+};
+
+export default userReducer;
