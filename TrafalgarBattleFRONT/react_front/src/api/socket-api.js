3b654d7a5c81adaecd174033ca555a4321f0596d
loicdvl <loic.devoille@hotmail.fr>
2017-06-14 21:55:04 +0200

    Front - socket integrated to redux

---
+++ TrafalgarBattleFRONT/react_front/src/api/socket-api.js
@@ -0,0 +1,7 @@
+import store from '../store';
+import { getSocketSuccess } from '../actions/socket-actions';
+
+export function setSocket(socket) {
+    return store.dispatch(getSocketSuccess({socket: socket}));
+}
+
